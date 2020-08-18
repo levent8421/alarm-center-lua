c9c81d3173ee7bc3c1849ddd395064481bdbeb34
levent8421 <levent8421@outlook.com>
2020-08-18 17:40:14 +0800

    First Commit

---
+++ file_utils.lua
@@ -0,0 +1,18 @@
+require 'io'
+
+function ReadFile(filename, openFlag)
+    local file = io.open(filename, openFlag)
+    local content = file.read('*a')
+    file:close()
+    return content
+end
+
+function ReadBinaryFile(filePath, filename)
+    local fullFileName = filePath .. filename
+    return ReadFile(fullFileName, 'rb')
+end
+
+function ReadTextFile(filePath, filename)
+    local fullFileName = filePath .. filename
+    return ReadFile(fullFileName, 'r')
+end
